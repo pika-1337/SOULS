463aaece72470e6199c5f45bfc8202f71e3deeee
pika-1337 <pika_sergiu@ukr.net>
2022-11-17 15:59:33 +0200

    added mint discover proof asked

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,34 @@
-$function({
+$(function(){
   
+
+  $('.discover-box').slick({
+  centerMode: true,
+  centerPadding: '80px',
+  slidesToShow: 3,
+  infinite: true,
+  dots: false ,
+  slidesToScroll: 1
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 3
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+})
+
+
 })
